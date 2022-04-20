bac998c940476a510df61e60f5238d580ba065de
JoseMacevo <devcount@outlook.es>
2022-04-20 11:59:07 +0200

    fucking_fighters_finished

--- venvwar/Scripts/two_fighters.py
+++ venvwar/Scripts/two_fighters.py
@@ -19,37 +19,34 @@ declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
   Harry attacks Lew; Lew now has 2 health.
   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
 
-class Fighter(object):
-    def __init__(self, name, health, damage_per_attack):
-        self.name = name
-        self.health = health
-        self.damage_per_attack = damage_per_attack
-        
-    def __str__(self): return "Fighter({}, {}, {})".format(self.name, self.health, self.damage_per_attack)
-    __repr__=__str__
 """
 
 from two_fighters_preloaded import Fighter
 
 
 def declare_winner(fighter_1, fighter_2, first_attacker):
-    
+    if first_attacker == fighter_1.name:
+        fighter_1 = fighter_1
+    else:
+        fighter_x = fighter_1
+        fighter_1 = fighter_2
+        fighter_2 = fighter_x
 
-    while fighter_1.health and fighter_2.health > 0:
-        fighter_1 = first_attacker
+    while fighter_1.health >= 0 and fighter_2.health >= 0:
         fighter_2.health = fighter_2.health - fighter_1.damage_per_attack
-        print(f"{fighter_1.name} attacks {fighter_2.name}; {fighter_2.name} now has {fighter_2.health} healths.")
-        fighter_1.health = fighter_1.health - fighter_2.damage_per_attack
-        print(f"{fighter_2.name} attacks {fighter_1.name}; {fighter_1.name} now has {fighter_1.health} healths.")
-        fighter_2.health = fighter_2.health - fighter_1.damage_per_attack
-        print(f"{fighter_1.name} attacks {fighter_2.name}; {fighter_2.name} now has {fighter_2.health} healths.")
-        fighter_1.health = fighter_1.health - fighter_2.damage_per_attack
-        print(f"{fighter_2.name} attacks {fighter_1.name}; {fighter_1.name} now has {fighter_1.health} health.")
-        fighter_2.health = fighter_2.health - fighter_1.damage_per_attack
-        print(f"{fighter_1.name} attacks {fighter_2.name}; {fighter_2.name} now has {fighter_2.health} health and he's dead.")
-        if fighter_1.health or fighter_2.health <= 0:
-            winner = fighter_1.name or fighter_2.name 
-            return winner
+        print(f"{fighter_1.name} attacks {fighter_2.name}; {fighter_2.name} now has {fighter_2.health}")
+        if fighter_2.health > 0:
+            fighter_1.health = fighter_1.health - fighter_2.damage_per_attack
+            print(f"{fighter_2.name} attacks {fighter_1.name}; {fighter_1.name} now has {fighter_1.health}")
+        elif fighter_1.health > 0: 
+            print(f"{fighter_2.name} is dead {fighter_1.name} Wins...")
+            return f"{fighter_1.name}"
+
+    if fighter_1.health <= 0:
+        return f"{fighter_2.name}"
+
+    if fighter_2.health <= 0:
+        return f"{fighter_1.name}"
 
 
-print(declare_winner(Fighter("Lew", 20, 2), Fighter("Harry", 5, 4), "Harry"))
+print(declare_winner(Fighter("Harold", 20, 5), Fighter("Harry", 5, 4), "Harry"))
--- venvwar/Scripts/two_fithers_tests.py
+++ venvwar/Scripts/two_fithers_tests.py
@@ -6,7 +6,7 @@ from two_fighters import declare_winner
 class test_basic_cases(unittest.TestCase):
     def test_cases(self):
         self.assertEqual(declare_winner(Fighter("Lew", 10, 2), Fighter ("Harry", 5, 4), "Lew"), "Lew")
-        self.assertEqual(declare_winner(Fighter("Lew", 20, 2), Fighter ("Harry", 5, 4), "Harry"), "Harry")
+        self.assertEqual(declare_winner(Fighter("Lew", 10, 2), Fighter ("Harry", 5, 4), "Harry"), "Harry")
         self.assertEqual(declare_winner(Fighter("Harold", 20, 5), Fighter ("Harry", 5, 4), "Harry"), "Harold")
         self.assertEqual(declare_winner(Fighter("Harold", 20, 5), Fighter ("Harry", 5, 4), "Harold"), "Harold")
         self.assertEqual(declare_winner(Fighter("Jerry", 30, 3), Fighter ("Harold", 20, 5), "Jerry",), "Harold")
